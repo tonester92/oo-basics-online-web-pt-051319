07009776ec4d9ee476fd6e8299a40640d1d4d196
Antonio Blandin <antonio.blandin25@gmail.com>
2019-05-27 22:57:34 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+ 
+  def initialize(brand)
+    @brand = brand
+  end
+ 
+  def condition_new
+    puts "The shoe has been repaired!"
+  end
+ 
+end
+
+shoe = Shoe.new("New")
+shoe.condition_new
